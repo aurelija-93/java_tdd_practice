b299fe7eaa2cff3fae5ea33a75ba6c323d3d2307
aurelija-93 <aurelija.bellerby@gmail.com>
2023-08-14 19:17:39 +0100

    passed Calculator tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,21 @@
 public class Calculator {
+    public int add(int firstNum, int secondNum) {
+        int result = firstNum + secondNum;
+        return result;
+    }
+
+    public int subtract(int firstNum, int secondNum) {
+        int result = firstNum - secondNum;
+        return result;
+    }
+
+    public int multiply(int firstNum, int secondNum) {
+        int result = firstNum * secondNum;
+        return result;
+    }
+
+    public double divide(double firstNum, double secondNum) {
+        double result = firstNum / secondNum;
+        return result;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
+    public Calculator calculator;
+
+    @Before
+    public void Calculator() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd() {
+        int result = calculator.add(2, 5);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void canSubtract() {
+        int result = calculator.subtract(5, 2);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void canMultiply() {
+        int result = calculator.multiply(2, 5);
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void canDivide() {
+        double result = calculator.divide(5, 2);
+        assertEquals(2.5, result, 0.01);
+    }
 }
